f06e948f719ff0b5a333c14a957ad398e3263a2c
dongtran <thiendong.iuh@gmail.com>
2023-09-29 00:12:20 +0700

    add page create password

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 import './models/token.dart';
-
 import 'login_page.dart';
 
 void main() async {
@@ -27,21 +26,21 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      routes: {
+        'secondPage': (context) => SecondPage(),
+      },
       home: FutureBuilder<bool>(
         future: checkLoginStatus(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            // Xử lý trường hợp đang kiểm tra trạng thái đăng nhập
             return const Scaffold(
               body: Center(
                 child: CircularProgressIndicator(),
               ),
             );
           } else if (snapshot.hasError) {
-            // Xử lý trường hợp có lỗi
             return Text('Error: ${snapshot.error}');
           } else {
-            // Xử lý trường hợp hoàn thành kiểm tra trạng thái đăng nhập
             final isLoggedIn = snapshot.data ?? false;
 
             return isLoggedIn
@@ -66,11 +65,11 @@ class MyApp extends StatelessWidget {
       if (result.exists) {
         final data = result.data();
         print('data: ${data}');
-        return true; // Đã đăng nhập thành công
+        return true;
       }
     }
 
-    return false; // Chưa đăng nhập
+    return false;
   }
 }
 
@@ -106,6 +105,61 @@ class _MyHomePageState extends State<MyHomePage> {
               style: const TextStyle(fontSize: 24),
             ),
           ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(
+              context, 'secondPage'); // Điều hướng đến trang mới
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Card(
+          elevation: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ListView(
+              children: <Widget>[
+                Text(
+                  'Application',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                TextFormField(),
+                SizedBox(height: 20),
+                Text(
+                  'Username/Email',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                TextFormField(),
+                SizedBox(height: 20),
+                Text(
+                  'Password',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                TextFormField(),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    // Xử lý khi nút hoàn thành được nhấn
+                  },
+                  child: Text('Hoàn thành'),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
